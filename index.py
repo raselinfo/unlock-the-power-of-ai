8405d5480b70287b3d003af3ecb971e0b2fecdee
raselSoftNode <rasel.softnode@gmail.com>
2023-03-30 22:49:37 +0600

    update

--- index.py
+++ index.py
@@ -1,13 +1,16 @@
 from langchain.llms import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
 import os
-os.environ['OPENAI_API_KEY']='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'
-llm = OpenAI(temperature=0.9,stop=["rasel"])
+os.environ['OPENAI_API_KEY']=os.getenv("OPEN_AI_API_KEY")
+# llm = OpenAI(temperature=0.9,stop=["rasel"])
 
-# print(llm("hello how are you?"))
-# def generate_serially():
-#     for _ in range(10):
-#         res=llm.generate(["Hello, how are you?"])
-#         print(res.generations)
-# generate_serially()
+# # print(llm("hello how are you?"))
+# # def generate_serially():
+# #     for _ in range(10):
+# #         res=llm.generate(["Hello, how are you?"])
+# #         print(res.generations)
+# # generate_serially()
 
-print(llm("Hello, how are you?"))
+# print(llm("Hello, how are you?"))
+
